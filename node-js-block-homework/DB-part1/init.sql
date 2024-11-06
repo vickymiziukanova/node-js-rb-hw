ca412222dd586d89c99d42d8ed23fb2d2a55bf45
vickymiziukanova <miziukanova.viktoriia@betterme.world>
2024-11-07 00:22:38 +0200

    fix: cascade delete

--- node-js-block-homework/DB-part1/init.sql
+++ node-js-block-homework/DB-part1/init.sql
@@ -10,9 +10,23 @@ CREATE TABLE IF NOT EXISTS UserPreferences (
     PreferredCommunication INT,
     UserId INT,
     FOREIGN KEY (UserId) REFERENCES Users(ID)
+    ON DELETE CASCADE
 );
 
+INSERT INTO Users (name, email)
+VALUES
+    ('John1', 'john@gmail.com'),
+    ('Alise2' , 'alice@gmail.com')
+RETURNING *;
 
+INSERT INTO UserPreferences (languagecode, preferredcommunication, userid)
+VALUES
+    ('fr', 123, 1),
+    ('fr', 123, 1),
+    ('de', 123, 2)
+RETURNING *;
+
+DELETE FROM Users WHERE id = 1;
 
 -- ALTER TABLE users ADD UNIQUE (name)
 -- ALTER TABLE users ALTER COLUMN name SET NOT NULL;
